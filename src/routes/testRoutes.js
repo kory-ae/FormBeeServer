04967cdfc1d335827959b0ad9e6193748da40260
Kory Karr <kory.activationenergy@gmail.com>
2025-01-24 13:02:04 -0700

    cleanup

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -2,10 +2,8 @@ import express from 'express';
 import logger from '../config/logger.js'
 
 const router = express.Router();
-
 const key = 'jasdf8234b1234VB'
 const getNow = () => new Date().toLocaleString()
-
 
 router.get(
     `/${key}/debug`,
